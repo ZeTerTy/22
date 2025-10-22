cf504275bf813a34c807ac94d88f8d7f19ee5694
ZeTerTy <prilepskyyaroslav@yandex.ru>
2025-10-22 18:53:08 +0700

    Update array_function.c

--- src/array_function.c
+++ src/array_function.c
@@ -1,4 +1,4 @@
-#include "../include/array_function.h"
+#include <stddef.h>
 
 void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     for (size_t i = 0; i < size_src1; i++) {
