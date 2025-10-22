4880f009b5e78a3b64ac8ea88f96c5827d8e105a
Прилепский Ярослав <prilepskyyaroslav@yandex.ru>
2025-10-22 14:43:23 +0300

    Completed homework5

--- src/array_function.c
+++ src/array_function.c
@@ -1,4 +1,4 @@
-#include "array_function.h"
+#include "../include/array_function.h"
 
 void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     for (size_t i = 0; i < size_src1; i++) {
@@ -44,4 +44,3 @@ void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
         }
     }
 }
-
